5bd50b106facb07aca90ee77d234841e22b6c37c
BagasDhitya <bagasdhityataufiqqi98@gmail.com>
2024-11-06 10:33:02 +0700

    chore: update application properties

--- src/main/java/com/example/funnycation/FunnycationApplication.java
+++ src/main/java/com/example/funnycation/FunnycationApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FunnycationApplication {
 	public static void main(String[] args) {
-		System.out.println("DATABASE_URL: " + System.getenv("DATABASE_URL"));
 		SpringApplication.run(FunnycationApplication.class, args);
 	}
 }
